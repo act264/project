0783d31ba0bd05e69163bc8ac4c956ea074ea270
Alex Berger <alexber@matrix.co.il>
2019-06-10 18:03:55 +0300

    06102019

--- src/main/java/il/co/yad2_new/yad2_auto_tests/infra/pages/AmazonLandingPage.java
+++ src/main/java/il/co/yad2_new/yad2_auto_tests/infra/pages/AmazonLandingPage.java
@@ -3,7 +3,7 @@ package il.co.yad2_new.yad2_auto_tests.infra.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class AmazonLandingPage {
+public class AmazonLandingPage{
 	
 	private WebDriver driver;
 	private  static final By searchBox = By.id("twotabsearchtextbox");

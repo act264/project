fe935207c973d0404403254df3bc3ca8cd1a1d7d
Alex Berger <alexber@matrix.co.il>
2019-06-16 17:11:22 +0300

    06162019

--- src/main/java/tests/Yad2Tests.java
+++ src/main/java/tests/Yad2Tests.java
@@ -1,9 +1,12 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByXPath;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import il.co.yad2_new.yad2_auto_tests.infra.pages.Yad2LandingPage;
 
@@ -19,15 +22,17 @@ public class Yad2Tests {
 			driver = new ChromeDriver();
 		}
 		}
-
+	@Test
 	public void _001_testSearch() {
 	driver.get("http://yad2.co.il");
 	Yad2LandingPage yad2LandingPage = new Yad2LandingPage(driver);
 	
-	String searchTerm = "Apple";
-	yad2LandingPage.wriToTheSearchbox(searchTerm);
+	//String searchTerm = "Apple";
+	//yad2LandingPage.wriToTheSearchbox(searchTerm);
 	
 	}
 	
-
+	public void clickButton() {
+		driver.findElement(By.xpath("//li[@class='no_sub_menu private_zone desktop_only']")).click();
+	}
 }
